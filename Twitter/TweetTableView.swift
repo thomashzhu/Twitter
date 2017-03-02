a68909e499930ab590c2b35e10d8d1d3bfe48379
thomashzhu <dev@thomaszhu.com>
2017-03-02 12:25:07 -0800

    Separate TweetView from TweetCell for reuse purposes

--- Twitter/TweetTableView.swift
+++ Twitter/TweetTableView.swift
@@ -74,7 +74,7 @@ class TweetTableView: UITableView, UITableViewDataSource, UITableViewDelegate, U
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let cell = tableView.dequeueReusableCell(withIdentifier: "TweetCell", for: indexPath) as? TweetCell {
-            configureUI(cell: cell, indexPath: indexPath)
+            configureUI(tweetView: cell.tweetView, indexPath: indexPath)
             return cell
         }
         
@@ -114,47 +114,47 @@ class TweetTableView: UITableView, UITableViewDataSource, UITableViewDelegate, U
      ====================================================================================================== */
     
     // MARK: Configure cell UI
-    private func configureUI(cell: TweetCell, indexPath: IndexPath) {
+    private func configureUI(tweetView: TweetView, indexPath: IndexPath) {
         
         let tweet = tweets[indexPath.row]
-        cell.tweet = tweet
+        tweetView.tweet = tweet
         
         // If the network is slow, it might take a while to load a retweeted user's name, so UI changes might
         // be delayed. For the following if block, it decides whether to show or hide the retweet status view
         // right away, WITHOUT waiting for a retweet user's name to be returned.
         if tweet.retweetCount > 0 {
-            cell.retweetedByLabel.text = "..."
-            cell.retweetStatusView.isHidden = false
+            tweetView.retweetedByLabel.text = "..."
+            tweetView.retweetStatusView.isHidden = false
         } else {
-            cell.retweetStatusView.isHidden = true
+            tweetView.retweetStatusView.isHidden = true
         }
         
         if let url = tweet.profileImageURL {
-            cell.profileImageView.setImageWith(url)
+            tweetView.profileImageView.setImageWith(url)
         }
         
-        cell.usernameLabel.text = tweet.name
+        tweetView.usernameLabel.text = tweet.name
         
-        cell.screenNameLabel.text = tweet.screenName
+        tweetView.screenNameLabel.text = tweet.screenName
         
         if let timestamp = tweet.timestamp {
-            cell.timestampLabel.text = TwitterClient.tweetTimeFormatted(timestamp: timestamp)
+            tweetView.timestampLabel.text = TwitterClient.tweetTimeFormatted(timestamp: timestamp)
         }
         
-        cell.tweetLabel.text = tweet.text
+        tweetView.tweetLabel.text = tweet.text
         
-        cell.cellHeightAdjustmentClosure = {
+        tweetView.cellHeightAdjustmentClosure = {
             self.reloadRows(at: [indexPath], with: .none)
         }
-        cell.determineRetweetStatusAndUpdateUI()
+        tweetView.determineRetweetStatusAndUpdateUI()
         
-        cell.replyButtonClosure = {
+        tweetView.replyButtonClosure = {
             let delegate = MessageViewDelegate(tableViewToBeReloadedUponCompletion: self, tweetInReplyTo: tweet)
             delegate.present(mode: .Reply)
         }
         
         if let favorited = tweet.favorited {
-            cell.configureFavoriteButton(favorited: favorited)
+            tweetView.configureFavoriteButton(favorited: favorited)
         }
     }
     /* ==================================================================================================== */
